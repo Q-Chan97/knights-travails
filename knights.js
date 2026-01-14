41f7534a5c084f8ebfb1af3de3290ea11cbc67d7
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-01-14 12:07:41 -0600

    Create traversal while loop for knightMoves

--- knights.js
+++ knights.js
@@ -2,6 +2,27 @@ function knightMoves(startSquare, endSquare) {
     if (isOutOfBounds(startSquare)) throw new Error("Your start coordinates are not in bounds."); // Guards for coordinates
     if (isOutOfBounds(endSquare)) throw new Error("Your end coordinates are out of bounds.")
 
+    let startNode = {position: startSquare, parent: null} // Starting spot for search, Object will keep track of node relationships and location
+    let visitedSquares = new Set();
+    let queue = [startNode]; // Queue starts with starting node
+    visitedSquares.add(startSquare.toString()); // Add starting square to visited nodes by default
+
+    while(queue.length > 0) {
+        let currentNode = queue.shift(); // Get current node in queue
+       
+        let possibleMoves = getLegalMoves(currentNode.position).toString(); // Get all legal moves from current node
+
+        possibleMoves.forEach((move) => { 
+            let moveString = move.toString();
+            if (!visitedSquares.has(moveString)) { // Add each move to visited nodes if not there already
+                visitedSquares.add(moveString); 
+                queue.push({ // Pushes new current node object into queue, tracking parent and position
+                    position: move,
+                    parent: currentNode
+                });
+            }
+        })
+    }
 }
 
 function isOutOfBounds([x, y]) { // Will return true if coordinate numbers are bigger than 7 or smaller than 0
